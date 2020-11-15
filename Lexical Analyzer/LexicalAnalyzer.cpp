9ede2fcabf647a3f173675ef5dcedf9d720e8f34
kluong2 <70615539+kluong2@users.noreply.github.com>
2020-11-15 11:50:06 -0500

    Small change to filenames(one of them was wrong).

--- Lexical Analyzer/LexicalAnalyzer.cpp
+++ Lexical Analyzer/LexicalAnalyzer.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	//Here is a string of all the textfiles that came with the project. I also added on my own "testing" textfile to test other conditions.
 	string filename[] = { "ab.txt", "ab2.txt", "bad_assignment.txt", "bad_decl.txt", "bad_exp.txt", "bad_exp2.txt","bad_exp3.txt","bad_exp4.txt","bad_exp5.txt","bad_exp6.txt",
-						"bad_exp7.txt", "bad_exp8.txt", "bad_scoping1.txt", "bad_scoping2.txt", "euclid.txt", "hiding.txt", "if.txt", "print.txt", "testing.txt" };
+						"bad_exp7.txt", "bad_exp8.txt", "bad_scoping.txt", "bad_scoping2.txt", "euclid.txt", "hiding.txt", "if.txt", "print.txt", "testing.txt" };
 
 	LexicalAnalyzer analyze(filename[18]);
 
